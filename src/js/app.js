9eb08cc2bd3c0a380ff09a867b3f1f1a6b32f5bb
JosefaMiller <josefamillerb@gmail.com>
2023-02-28 22:38:51 +0000

    Definiendo estilos

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,23 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Name" + " "} ${variables.lastname ||
+    "Last name"}</h1>
+          <h3>${variables.role || "Role"} <br> ${variables.city ||
+    "City" + ", "} ${variables.country || "Country"}</h3>
+          <ul class="position-right" ${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +63,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTLR1_lzXgk0ir84TEZTFa_2s0udqe77xQOAtoYYLl74ynogsu-",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/516641ff-9053-4014-97e7-b40310fb1d77-profile_image-150x150.png",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
